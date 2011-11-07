622bb3597495a4b1238113b7e542f392e4a70f11
Kumar Ashwani <Kumartheashwani@gmail.com>
2011-11-07 15:56:59 +0530

    version 2

--- src/javaapplication1/JavaApplication1.java
+++ src/javaapplication1/JavaApplication1.java
@@ -15,7 +15,7 @@ public class JavaApplication1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.print("Ashwani");
+        System.out.print("Ashwani Kumar");
         
         
     }
